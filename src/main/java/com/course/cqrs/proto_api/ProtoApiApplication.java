e9d7d19555fc2a793974e5eda51cd7e90380181d
odevpedro <pedrosschmidt2@gmail.com>
2025-11-07 22:24:07 -0300

    initial files

---
+++ src/main/java/com/course/cqrs/proto_api/ProtoApiApplication.java
@@ -0,0 +1,13 @@
+package com.course.cqrs.proto_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoApiApplication.class, args);
+	}
+
+}
